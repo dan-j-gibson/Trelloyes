53cd748b61ef3743f2ba2e06716b1f2ab93d61ea
Dan Gibson <dgibson1124@gmail.com>
2020-10-27 14:26:35 -0700

    Add CSS files

--- src/List.js
+++ src/List.js
@@ -1,21 +1,28 @@
 import React from 'react';
+import Card from './Card'
 import './List.css';
-import Card from './Card';
 
-export default function list(props){
-    return (
-        <section className='List'>
-            <header className='List-header'>
-                <h1>{props.header}</h1>
-            </header>
-            <div className='List-cards'>
-            {props.cards.map((card) =>
+export default function List(props) {
+  return (
+    <section className='List'>
+      <header className='List-header'>
+        <h2>{props.header}</h2>
+      </header>
+      <div className='List-cards'>
+        {props.cards.map((card) =>
           <Card
+            key={card.id}
             title={card.title}
-            content={card.content}key={card.id}
+            content={card.content}
           />
         )}
-            </div>
-        </section>
-    )
+        <button
+          type='button'
+          className='List-add-button'
+        >
+          + Add Random Card
+        </button>
+      </div>
+    </section>
+  )
 }
