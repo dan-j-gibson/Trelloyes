4b524da5bd5619c861615195e95bebf1cc2aaeb5
Dan Gibson <dgibson1124@gmail.com>
2020-10-27 12:41:22 -0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,26 +1,46 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import List from './List';
 import './App.css';
-import STORE from './store.js'
+import STORE from './store.js';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+class App extends Component {
+  static defaultProps = {
+    store: {
+      lists: [],
+      allCards: {},
+    }
+  };
+    render () { 
+        return (
+      <main className='App-main'>
+        <header className='App-header'>
+          <h1>Trelloyes!</h1>
+          <div className='App-list'>
+            <App 
+            lists={STORE.lists}
+            id={STORE.allCards}
+          </div>
+        </header>
+      </main>
+    );
+  }
 
 export default App;
+
+
+    // <div className="App">
+    //   <header className="App-header">
+    //     <img src={logo} className="App-logo" alt="logo" />
+    //     <p>
+    //       Edit <code>src/App.js</code> and save to reload.
+    //     </p>
+    //     <a
+    //       className="App-link"
+    //       href="https://reactjs.org"
+    //       target="_blank"
+    //       rel="noopener noreferrer"
+    //     >
+    //       Learn React
+    //     </a>
+    //   </header>
+    // </div>
